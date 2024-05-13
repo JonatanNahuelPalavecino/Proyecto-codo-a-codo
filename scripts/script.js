952ea74881e8b5132278aa5c9c9f7ff06afef8cb
Alejo <alejodiazjato00@gmail.com>
2024-05-13 20:07:09 -0300

    validacion del form de contacto

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const contacto_form = document.querySelector(".contacto-formulario-form");
+const form_nombre = document.querySelector("#nombre");
+const form_telefono = document.querySelector("#telefono");
+const form_email = document.querySelector("#email");
+const form_mensaje = document.querySelector("#mensaje");
+
+contacto_form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  form_nombre.value = form_nombre.value.trim();
+
+  const valida_telefono = Number(form_telefono.value.trim());
+
+  if (isNaN(valida_telefono)) {
+    alert("Asegurate de ingresar sólo números en tu teléfono");
+  }
+});
